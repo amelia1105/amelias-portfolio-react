6a712bc2b553c4703f4101269a8dbf267867eaa8
Amelia <aebellanger@yahoo.com>
2025-01-27 15:23:31 -0500

    Incorporate Bootstrap styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
-import '../styles/Footer.css';
-
 function Footer() {
-    return (
-      <footer className="footer">
-        <p>Footer goes here</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer style={{ backgroundColor: '#573842', height: '80px', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }} className="footer fixed-bottom">
+      <p>Footer goes here</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 import Navigation from './Navigation';
-import '../styles/Header.css';
 
 function Header() {
-    return (
-      <header className="header">
-        <h1>Welcome to my website! 😊</h1>
-        <h2>Amelia Alvarado</h2>
-        <Navigation className="nav" />
-      </header>
-    );
-  }
+  return (
+    <header className="header text-center" style={{ backgroundColor: '#B9929F' }}>
+      <h1 className="text-dark-pink pt-5 display-1" style={{ fontFamily: 'tahoma, serif', color: '#693A52' }}>Amelia Alvarado</h1>
+      <h3 className="text-dark">Welcome to my website! 😊</h3>
+      <Navigation />
+    </header>
+  );
+}
   
-  export default Header;
+export default Header;
   
