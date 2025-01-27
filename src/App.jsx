68928bfefa8eacfcc39dfd16fa76bad3d76f4851
Amelia <aebellanger@yahoo.com>
2025-01-26 19:40:26 -0500

    Add styles folder and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './styles/App.css';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="mx-3">
+      <main className="section">
         <Outlet />
       </main>
       <Footer />
