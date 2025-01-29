6088a162b9ff88149f0ae3287a9b3ef684f93a20
Amelia <aebellanger@yahoo.com>
2025-01-28 20:47:56 -0500

    Add image and text to About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,27 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+
 export default function About() {
   return (
     <div>
       <h1>About Me</h1>
-      <p>
-        This is my About Me page. I am a full stack web developer. I have experience with HTML, CSS, JavaScript, Node.js, Express.js, SQL, React, and more. I am excited to use my skills to help you build your next project.
-      </p>
+      <Container>
+        <Row className="p-2">
+          <Col xs={12} sm={12} md={6} lg={5} className="d-flex justify-content-center mb-3 mb-md-0">
+            <Image src="../assets/grad-photo.jpg" roundedCircle width={300} height={300} />
+          </Col>
+          <Col xs={12} sm={12} md={6} lg={7} className="d-flex justify-content-center align-items-center">
+            <p>
+              Hello! My name is Amelia Alvarado. I live in North Carolina and have a BS in Environmental Science and a MS in Data Analytics. 
+              I am currently pursuing a certificate in a coding bootcamp at Rutgers University. I have experience with HTML, CSS, JavaScript, Node.js, Express.js, 
+              PostgreSQL, React, and more. I enjoy working with data and hope to use my skills in a development setting. I am excited to use my 
+              skills to help you build your next project.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
