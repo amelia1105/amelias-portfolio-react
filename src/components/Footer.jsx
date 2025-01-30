8c93286c479ffd056ca9c768c53a597c68a0d1bc
Amelia <aebellanger@yahoo.com>
2025-01-30 18:24:03 -0500

    Add icons and links to footer, fix About Me page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,29 @@
+import Nav from 'react-bootstrap/Nav';
+
 function Footer() {
   return (
-    <footer style={{ backgroundColor: '#573842', height: '80px', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }} className="footer fixed-bottom">
-      <p>Footer goes here</p>
-    </footer>
+    <Nav
+      className="footer justify-content-center"
+      activeKey="/home"
+      onSelect={(selectedKey) => window.open(selectedKey, '_blank')}
+      style={{ position: 'fixed', bottom: 0, width: '100%', backgroundColor: '#573842' }}
+    >
+      <Nav.Item>
+        <Nav.Link eventKey="https://stackoverflow.com/users/29439479/amelia-bellanger-alvarado">
+          <img src="./assets/stack-overflow-logo.png" alt="Stack Overflow logo" style={{ width: '30px' }} />
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="https://www.linkedin.com/in/amelia-alvarado-691507297/">
+          <img src="./assets/linkedin-logo.png" alt="LinkedIn logo" style={{ width: '30px' }} />
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="https://github.com/amelia1105">
+          <img src="./assets/github-logo.png" alt="GitHub logo" style={{ width: '30px' }} />
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
 
